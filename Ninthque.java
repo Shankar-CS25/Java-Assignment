c054c8030a09fcf0746f01f5878c71e0d58a81d7
Shankar-CS25 <chinnari.shankar_cs25@gla.ac.in>
2026-02-06 19:27:08 +0530

    Rename class from 'ninthque' to 'Ninthque'

--- Ninthque.java
+++ Ninthque.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ninthque {
+public class Ninthque {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -12,3 +12,4 @@ public class ninthque {
         System.out.println(result);
     }
 }
+
