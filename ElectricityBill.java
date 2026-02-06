fadf1a5e98e88f222bbbfab5e952d75234501545
Shankar-CS25 <chinnari.shankar_cs25@gla.ac.in>
2026-02-06 19:24:03 +0530

    Rename class to follow Java naming conventions

--- ElectricityBill.java
+++ ElectricityBill.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class electricitybill {
+public class ElectricityBill {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -20,3 +20,4 @@ public class electricitybill {
         System.out.println("Total Bill = " + bill);
     }
 }
+
