448fb85cd5e3a18136e15e427825fb4981fbf211
Shankar-CS25 <chinnari.shankar_cs25@gla.ac.in>
2026-02-06 19:29:03 +0530

    Rename class from 'pattern' to 'Pattern'

--- Pattern.java
+++ Pattern.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class pattern {
+public class Pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter n: ");
@@ -13,3 +13,4 @@ public class pattern {
         }
     }
 }
+
