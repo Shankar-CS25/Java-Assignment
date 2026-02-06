5951d037f0790fe6ffdfe06e101aeb1679f12d86
Shankar-CS25 <chinnari.shankar_cs25@gla.ac.in>
2026-02-06 19:22:13 +0530

    Rename class to ClassCalculator and fix casing

--- ClassCalculator.java
+++ ClassCalculator.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class classcalculator {
+public class ClassCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -23,3 +23,4 @@ public class classcalculator {
         }
     }
 }
+
